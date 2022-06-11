ddd4a44b7be54f97d1722e2666d873a27028d350
Andrew Speth <speth.andrew@yahoo.com>
2022-06-11 16:49:53 -0600

    test: weather_service

---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'WeatherService' do
+  it 'returns forecast information', :vcr do
+    lat_long = MapquestService.get_lat_long("denver,co")
+    response = WeatherService.get_weather(lat_long[:lat], lat_long[:lng])
+
+    expect(response).to be_a(Hash)
+    expect(response).to have_key(:current)
+    expect(response).to have_key(:daily)
+    expect(response).to have_key(:hourly)
+  end
+end
