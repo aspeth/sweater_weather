be47dcc4c83baa98aeedd1c42dcfc0668f192bc8
Andrew Speth <speth.andrew@yahoo.com>
2022-06-10 20:12:17 -0600

    initial test setup, route, and controller

---
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "weather API" do
+  it "sends the weather for a city" do
+    get '/api/v1/forecast?location=denver,co'
+    
+    expect(response).to be_successful
+  end
+end
