8efb568fd3c0ba9794b930258a9066f73d3d9257
Andrew Speth <speth.andrew@yahoo.com>
2022-06-12 16:00:19 -0600

    test/feat: error handling if email taken

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.hex(10))
-    render json: UsersSerializer.create_new(user)
+    if User.exists?(email: params[:email])
+      render json: { message: "email already taken" }, status: 400
+    else
+      user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.hex(10))
+      render json: UsersSerializer.create_new(user)
+    end
   end
 end
--- spec/requests/api/v1/registration_request_spec.rb
+++ spec/requests/api/v1/registration_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'user registration' do
-  it 'accepts user info and returns api key' do
+  it 'accepts user info and returns api key', :vcr do
     data = {
               "email": "whatever@example.com",
               "password": "password",
@@ -9,14 +9,14 @@ RSpec.describe 'user registration' do
             }
 
     post "/api/v1/users", params: data, as: :json
-
+    
     expect(response).to be_successful
     
     user = JSON.parse(response.body, symbolize_names: true)
-
+    
     expect(user).to have_key(:data)
     expect(user.keys.count).to eq(1)
-
+    
     expect(user[:data]).to be_a(Hash)
     expect(user[:data]).to have_key(:type)
     expect(user[:data][:type]).to eq("users")
@@ -25,13 +25,33 @@ RSpec.describe 'user registration' do
     expect(user[:data]).to have_key(:attributes)
     expect(user[:data][:attributes]).to be_a(Hash)
     expect(user[:data].keys.count).to eq(3)
-
+    
     user_info = user[:data][:attributes]
-
+    
     expect(user_info.keys.count).to eq(2)
     expect(user_info).to have_key(:email)
     expect(user_info[:email]).to eq(data[:email])
     expect(user_info).to have_key(:api_key)
     expect(user_info[:api_key]).to be_a(String)
+  end
+  
+  it "sends an error mesage if email is already taken", :vcr do
+    data = {
+              "email": "whatever@example.com",
+              "password": "password",
+              "password_confirmation": "password"
+            }
+  
+    post "/api/v1/users", params: data, as: :json
+
+    data = {
+              "email": "whatever@example.com",
+              "password": "password",
+              "password_confirmation": "password"
+            }
+  
+    post "/api/v1/users", params: data, as: :json
+    
+    expect(response.status).to eq(400)
   end
 end
