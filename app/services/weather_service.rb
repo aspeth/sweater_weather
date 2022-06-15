d39c2137cf7efd3d8a1bfb6471cfc45af93b3603
Andrew Speth <speth.andrew@yahoo.com>
2022-06-14 19:58:43 -0600

    refactor services to inherit methods from base_service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,7 +1,6 @@
-class WeatherService
+class WeatherService < BaseService
   def self.get_weather(lat, long)
-    conn = Faraday.new("https://api.openweathermap.org/data/2.5/")
-    response = conn.get("onecall?lat=#{lat}&lon=#{long}&appid=#{ENV["openweather_api_key"]}&units=imperial")
-    JSON.parse(response.body, symbolize_names: true)
+    response = conn("https://api.openweathermap.org/data/2.5/").get("onecall?lat=#{lat}&lon=#{long}&appid=#{ENV["openweather_api_key"]}&units=imperial")
+    get_json(response)
   end
 end
